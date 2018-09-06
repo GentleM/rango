8cb0e7b1a0e240fc7e7a55aa2ce6b77c17319f91
chenjiman <chenjiman@thunisoft.com>
2018-09-06 17:13:49 +0800

    新增文件

---
+++ src/main/java/com/cjm/rango/RangoApplication.java
@@ -0,0 +1,12 @@
+package com.cjm.rango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cjm/rango/RangoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjm.rango;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RangoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
