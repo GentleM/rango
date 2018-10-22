0f9f8a23be0296e10a906e1b349a3853e165c987
Jungle93 <1045340064@qq.com>
2018-10-22 10:29:08 +0800

    1、添加logback配置文件； 2、RedisCacheService添加LOGGER，格式化代码。 3、RangoApplication格式化，移除多余import。

--- src/main/java/com/cjm/rango/cache/RedisCacheService.java
+++ src/main/java/com/cjm/rango/cache/RedisCacheService.java
@@ -1,6 +1,7 @@
 package com.cjm.rango.cache;
 
-import com.cjm.rango.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,19 +11,25 @@ import org.springframework.stereotype.Component;
  * 项目启动，redis加载缓存数据。
  *
  * @author rango
- *         2018-09-11 0:10
+ * 2018-09-11 0:10
  **/
 @Component
-public class RedisCacheService implements CommandLineRunner{
-//    private Logger logger = lo
+public class RedisCacheService implements CommandLineRunner {
+
+    /**
+     * 日志。
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisCacheService.class);
 
     @Autowired
     private StringRedisTemplate StringRedisTemplate;
 
     @Override
-    public void run(String... strings) throws Exception {
+    public void run(String... strings) {
+        LOGGER.info("[初始化数据]开始......");
         StringRedisTemplate.delete(StringRedisTemplate.keys("*"));      //清空缓存数据
         StringRedisTemplate.opsForValue().set("aaa", "rango");
         StringRedisTemplate.opsForValue().set("xinzeng", "222");
+        LOGGER.info("[初始化数据]结束......");
     }
 }
